efbd80f025729e70e7b9a426fb6179ab50385d6b
Helio Fidalgo <hmnff@hotmail.com>
2017-12-18 13:13:48 -0200

    Teste finalizado com o projeto e atualizado as Libs do SyncFusion para  a ultima versão.

---
+++ ExpandableListView/ExpandableListView/Food.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpandableListView
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ ExpandableListView/ExpandableListView/FoodImageCell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExpandableListView
+{
+    class FoodImageCell : ImageCell
+    {
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            var food = (Food)BindingContext;
+        }
+    }
+}
